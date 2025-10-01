43ddf8694a098ddc4f892c0c0f3cc224765e42ec
Timofey Ivankov <playboicartina@gmail.com>
2025-10-01 20:11:15 +0300

    убрал рандомный генератор чисел

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -1,27 +1,35 @@
 package org.example.app;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.example.dinner.*;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Properties props = new Properties();
+        props.load(new FileInputStream("src/main/config.properties"));
+
+        int iterations = Integer.parseInt(props.getProperty("iterations", "10"));
+        int maxProgrammers = Integer.parseInt(props.getProperty("max_programmers", "1000"));
+        int fixedProgrammers = Integer.parseInt(props.getProperty("programmers_count", "-1"));
+        int fixedFood = Integer.parseInt(props.getProperty("food_count", "-1"));
+
         Random rand = new Random();
-        int MAX = 1000; // hehe messenger max is everywhere
-        // Running 10 iterations
-        for (int i = 0; i < 10; i++) {
-            // Generating random nums
-            int programmersCount = rand.nextInt(MAX);
-            int foodCounter = programmersCount + 1 + rand.nextInt(MAX - programmersCount);
+
+        for (int i = 0; i < iterations; i++) {
+            int programmersCount = (fixedProgrammers >= 0) ? fixedProgrammers : rand.nextInt(maxProgrammers);
+            int foodCounter = (fixedFood >= 0) ? fixedFood : programmersCount + 1 + rand.nextInt(maxProgrammers - programmersCount);
             AtomicInteger foodCount = new AtomicInteger(foodCounter);
+
             System.out.println("--------------------------------------------------------------------");
-            // Displaying them
             System.out.println("Programmers: " + programmersCount);
             System.out.println("Food: " + foodCounter);
-            // Initializing the dinner
+
             Dinner dinner = new Dinner();
-            // Starting the dinner
             dinner.serve(programmersCount, foodCount);
         }
     }
